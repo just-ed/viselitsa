3b6fd2be0be215e11c92e71023fa4878955912aa
just-ed <ed_tomeyan@mail.ru>
2019-03-01 23:25:38 +0300

    unicode_utils

--- viselitsa.rb
+++ viselitsa.rb
@@ -1,6 +1,7 @@
 require_relative 'lib/game'
 require_relative 'lib/result_printer'
 require_relative 'lib/word_reader'
+require 'unicode/utils'
 
 VERSION = 'Игра "Виселица", версия 5. (с) Хороший программист'
 
